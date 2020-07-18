f67eb519d925926fc44f2ef4311597570ad3188a
Sailaja-Viyyapu <68277518+Sailaja-Viyyapu@users.noreply.github.com>
2020-07-18 14:32:11 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'Women Security'
+copyright = '2020, Sailaja'
+author = 'Sailaja'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
